5fa24d66a7fc8ce298777b02b050836cb5e36322
srija1100 <2320030093@klh.edu.in>
2024-09-14 13:02:44 +0530

    Delete Week5 Postlab directory

--- Week5 Postlab/src/subject/com/Observer.java
+++
@@ -1,5 +0,0 @@
-package subject.com;
-
-public interface Observer {
-	 void update(String event);
-}
--- Week5 Postlab/src/subject/com/Subject.java
+++
@@ -1,7 +0,0 @@
-package subject.com;
-
-public interface Subject {
-	void addObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers(String event);
-}
